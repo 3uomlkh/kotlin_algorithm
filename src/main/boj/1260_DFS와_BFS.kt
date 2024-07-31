b8bdb2e7958fbd96c80b6657d111632d0cbeecdd
3uomlkh <cw1205@duksung.ac.kr>
2024-07-31 12:07:11 +0900

    add: 백준 DFS/BFS - 1260번 DFS와 BFS

---
+++ src/main/boj/1260_DFS와_BFS.kt
@@ -0,0 +1,22 @@
+fun main() {
+    val (n, m, v) = readln().split(" ").map { it.toInt() }
+    val graph = Array(n + 1) { IntArray(n + 1) }
+    var visited = ArrayList<Int>()
+
+    repeat(m) {
+        val (x, y) = readln().split(" ").map { it.toInt() }
+        // 양방향 그래프이므로 서로 이어주기
+        graph[x][y] = 1
+        graph[y][x] = 1
+    }
+
+}
+
+fun dfs(start: Int, graph: Array<IntArray>, visited: ArrayList<Int>) {
+    visited.add(start)
+    
+}
+
+fun bfs(start: Int, graph: Array<IntArray>, visited: ArrayList<Int>) {
+
+}
