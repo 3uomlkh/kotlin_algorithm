bb7ac3e4b8c46dad1b115fa859c1c39b641afd69
3uomlkh <cw1205@duksung.ac.kr>
2024-07-31 12:06:35 +0900

    add: 백준 구현 - 1475번 방 번호

---
+++ src/main/boj/1475_방_번호.kt
@@ -0,0 +1,24 @@
+fun main() {
+    val n = readln().toCharArray()
+    var arr = IntArray(10)
+    var count = 1
+    for (i in n.indices) {
+        val num = Character.getNumericValue(n[i])
+        if (arr[num] == 1) {
+            if (num == 6 && arr[9] == 0) {
+                arr[9] = 1
+            } else if (num == 9 && arr[6] == 0) {
+                arr[6] = 1
+            } else if (arr[6] == 1 && arr[9] == 1) {
+                count++
+                arr = IntArray(10)
+            }
+            else {
+                count++
+            }
+        } else {
+            arr[num] = 1
+        }
+    }
+    println(count)
+}
