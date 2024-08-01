697b539ff65e50ffd306a1e63574bec0627c7d5b
3uomlkh <cw1205@duksung.ac.kr>
2024-08-01 18:41:19 +0900

    refactor: 리트코드 13번 - Roman to Integer(다른 방식으로 풀기)

--- src/main/rtc/easy/roman_to_integer.kt
+++ src/main/rtc/easy/roman_to_integer.kt
@@ -7,43 +7,24 @@ fun main() {
 }
 
 fun romanToInt(s: String): Int {
-    val map = HashMap<Char, Int>()
-    map['I'] = 1
-    map['V'] = 5
-    map['X'] = 10
-    map['L'] = 50
-    map['C'] = 100
-    map['D'] = 500
-    map['M'] = 1000
+    val map = mutableMapOf(
+        'I' to 1,
+        'V' to 5,
+        'X' to 10,
+        'L' to 50,
+        'C' to 100,
+        'D' to 500,
+        'M' to 1000
+    )
 
     var answer = 0
-    for (i in s.indices) {
-        if (map.containsKey(s[i])) {
-            if (i - 1 >= 0 && s[i - 1] == 'I') {
-                if (s[i] == 'V' || s[i] == 'X') {
-                    answer--
-                    answer += map[s[i]]!! - 1
-                } else {
-                    answer += map[s[i]]!!
-                }
-            } else if (i - 1 >= 0 && s[i - 1] == 'X') {
-                if (s[i] == 'L' || s[i] == 'C') {
-                    answer -= 10
-                    answer += map[s[i]]!! - 10
-                } else {
-                    answer += map[s[i]]!!
-                }
-            } else if (i - 1 >= 0 && s[i - 1] == 'C') {
-                if (s[i] == 'D' || s[i] == 'M') {
-                    answer -= 100
-                    answer += map[s[i]]!! - 100
-                } else {
-                    answer += map[s[i]]!!
-                }
-            } else {
-                answer += map[s[i]]!!
-            }
-        }
+    var last = 1000
+
+    s.forEach {
+        val value = map[it] ?: 0
+        if (value > last) answer -= last * 2
+        answer += value
+        last = value
     }
 
     return answer
