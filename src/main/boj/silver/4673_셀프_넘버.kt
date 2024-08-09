10dc5f33a9d20b85d47e2edb19604fa1443d9319
3uomlkh <cw1205@duksung.ac.kr>
2024-08-10 00:15:23 +0900

    add: 백준 구현 - 4673번 셀프 넘버

--- src/main/boj/silver/4673_셀프_넘버.kt
+++ src/main/boj/silver/4673_셀프_넘버.kt
@@ -1,5 +1,29 @@
 package silver
 
 fun main() {
+    for (i in 1..10000) {
+        if (isSelfNumber(i)) {
+            println(i)
+        }
+    }
+}
 
+fun isSelfNumber(num: Int): Boolean {
+    for (i in 1..num) {
+        if (i < 10) {
+            if (i + i == num) return false
+        } else {
+            var selfNum = i
+            if (i < 100) {
+                selfNum += (i / 10) + (i % 10)
+            } else if (i < 1000) {
+                selfNum += (i / 100) + ((i % 100) / 10) + ((i % 100) % 10)
+            } else if (i < 10000) {
+                selfNum += (i / 1000) + ((i % 1000) / 100) + (((i % 1000) % 100) / 10) + (((i % 1000) % 100) % 10)
+            }
+            if (selfNum == num) return false
+        }
+    }
+
+    return true
 }
